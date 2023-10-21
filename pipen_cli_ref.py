f7973f38cc5b033ec3bbcd8f11d9a7097a20bd4b
pwwang <1188067+pwwang@users.noreply.github.com>
2023-10-21 05:51:38 +0000

    🔖 0.0.2

--- pipen_cli_ref.py
+++ pipen_cli_ref.py
@@ -25,7 +25,7 @@ if TYPE_CHECKING:
     from pipen import Proc
     from pipen_annotate.sections import Mixin
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 args = argx.ArgumentParser(
     description=("Make API reference documentation for pipen pipeline/processes.")
